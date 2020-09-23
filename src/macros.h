87418d9c58189c83af41fb254bf470d1e8d34ee2
Adrien Burgun <adrien.burgun@orange.fr>
2020-09-23 09:27:28 +0200

    :sparkles: macros update

--- src/macros.h
+++ src/macros.h
@@ -4,24 +4,65 @@
 #define MACROS_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
-// Error-checking input
+// The maximum line length that `INPUT(...)` accepts
+#define INPUT_MAX_LEN 1024
+
+/** Error-checking user input
+    Executes `prompt` every time the user is asked to enter the different values.
+    `err_prompt` will also be executed after the first, failed attempt.
+
+    @param prompt - the expression to run before accepting user input
+    @param err_prompt - the expression to run after an invalid input
+    @param n_args - the number of
+    @throw 2 if the input stream ends
+
+    ## Example
+
+    ```
+    int favorite_number = 0;
+    INPUT(
+        printf("Please enter your favorite number: "),
+        printf("This isn't a number!\n"),
+        1,
+        "%d",
+        &favorite_number
+    );
+    ```
+**/
 #define INPUT(prompt, err_prompt, n_args, ...) { \
-    int m_res; \
     prompt; \
-    while ((m_res = scanf(__VA_ARGS__)) != n_args) { \
-        if (m_res == EOF) { \
-            exit(2); \
+    while (1) { \
+        int m_res; \
+        char* f_res; \
+        char raw_str[INPUT_MAX_LEN + 1]; \
+        f_res = fgets(raw_str, INPUT_MAX_LEN, stdin); \
+        if (f_res == NULL || feof(stdin)) exit(2); \
+        m_res = sscanf(raw_str, __VA_ARGS__); \
+        if (m_res == n_args) { \
+            break; \
+        } else { \
+            err_prompt; \
+            prompt; \
         } \
-        scanf("%*[^\n]"); \
-        err_prompt; \
-        prompt; \
     } \
 };
 
 // Assertions
-#define ASSERT(cond) {if (!(cond)) {printf("Assertion error: the requirement '" #cond "' was not met\n"); exit(1);}}
-#define ASSERT_MSG(cond, ...) {if (!(cond)) {printf("Assertion error: "); printf(__VA_ARGS__); exit(1);}}
+
+/** Basic assertion, exits with code 1 is the condition was not met and prints an error generated using the contents of `cond`.
+
+    @param cond - The condition of the assertion; throws iff this condition yields false
+    @param printf_args... - The arguments passed to printf
+**/
+#define ASSERT(cond) {if (!(cond)) {fprintf(stderr, "Assertion error: the requirement '" #cond "' was not met\n"); exit(1);}}
+
+/** Assertion with a custom message; exits with code `code` and passes the remaining arguments to printf
+
+    @param cond - The condition of the assertion; throws iff this condition yields false
+    @param code - The exit code passed to `exit(...)`
+    @param printf_args... - The arguments passed to printf
+**/
+#define ASSERT_MSG(cond, code, ...) {if (!(cond)) {fprintf(stderr, "Assertion error: "); fprintf(stderr, __VA_ARGS__); exit(code);}}
 
 #endif
